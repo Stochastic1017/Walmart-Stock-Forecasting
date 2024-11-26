fa28c31a65d1c96ddef1240a16719594d4b43ddf
Shrivats Sudhir <133294122+Stochastic1017@users.noreply.github.com>
2024-11-26 15:49:34 -0600

    Add files via upload

--- R/Fit_ARCH_GARCH.R
+++ R/Fit_ARCH_GARCH.R
@@ -18,7 +18,7 @@ WMT_df <- na.omit(read.csv("Walmart_AdjPrice.csv"))
 WMT_df$Date <- as.Date(WMT_df$Date, format = "%Y-%m-%d")
 
 # Fit garch(1, 0) model to log returns
-fit_arch <- garchFit(~ garch(1, 0), data = WMT_df$LogReturns, cond.dist = "std")
+fit_arch <- garchFit(~ garch(1, 0), data = WMT_df$LogReturns, cond.dist = "norm")
 print(summary(fit_arch))
 
 # Title:
@@ -73,94 +73,9 @@ print(summary(fit_arch))
 # Information Criterion Statistics:
 #   AIC       BIC       SIC      HQIC 
 # -5.987829 -5.967716 -5.987863 -5.980173 
-# 
-# 
-# Series Initialization:
-#   ARMA Model:                arma
-# Formula Mean:              ~ arma(0, 0)
-# GARCH Model:               garch
-# Formula Variance:          ~ garch(1, 1)
-# ARMA Order:                0 0
-# Max ARMA Order:            0
-# GARCH Order:               1 1
-# Max GARCH Order:           1
-# Maximum Order:             1
-# Conditional Dist:          std
-# h.start:                   2
-# llh.start:                 1
-# Length of Series:          970
-# Recursion Init:            mci
-# Series Scale:              0.01508381
-# 
-# Parameter Initialization:
-#   Initial Parameters:          $params
-# Limits of Transformations:   $U, $V
-# Which Parameters are Fixed?  $includes
-# Parameter Matrix:
-#   U           V     params includes
-# mu     -0.24252318   0.2425232 0.02425232     TRUE
-# omega   0.00000100 100.0000000 0.10000000     TRUE
-# alpha1  0.00000001   1.0000000 0.10000000     TRUE
-# gamma1 -0.99999999   1.0000000 0.10000000    FALSE
-# beta1   0.00000001   1.0000000 0.80000000     TRUE
-# delta   0.00000000   2.0000000 2.00000000    FALSE
-# skew    0.10000000  10.0000000 1.00000000    FALSE
-# shape   1.00000000  10.0000000 4.00000000     TRUE
-# Index List of Parameters to be Optimized:
-#   mu  omega alpha1  beta1  shape 
-# 1      2      3      5      8 
-# Persistence:                  0.9 
-# 
-# 
-# --- START OF TRACE ---
-#   Selected Algorithm: nlminb 
-# 
-# R coded nlminb Solver: 
-#   
-#   0:     1144.0365: 0.0242523 0.100000 0.100000 0.800000  4.00000
-# 1:     1140.8189: 0.0242527 0.0907284 0.0976184 0.793654  3.99981
-# 2:     1139.9958: 0.0242539 0.0801877 0.100110 0.789829  3.99972
-# 3:     1139.4862: 0.0242560 0.0801669 0.110852 0.793895  3.99989
-# 4:     1139.1930: 0.0242596 0.0704237 0.116546 0.791757  3.99993
-# 5:     1138.9625: 0.0242735 0.0694686 0.127116 0.796087  4.00041
-# 6:     1138.9471: 0.0243531 0.0662979 0.134805 0.789117  4.00226
-# 7:     1138.9036: 0.0244474 0.0722051 0.138167 0.780925  4.00413
-# 8:     1138.8905: 0.0246555 0.0706010 0.133614 0.785691  4.00765
-# 9:     1138.8901: 0.0246560 0.0707493 0.133643 0.785784  4.00766
-# 10:     1138.8899: 0.0246617 0.0707840 0.133276 0.785777  4.00776
-# 11:     1138.8895: 0.0246705 0.0708291 0.133249 0.785972  4.00792
-# 12:     1138.8814: 0.0251670 0.0671673 0.129505 0.793530  4.01699
-# 13:     1138.8050: 0.0315006 0.0681307 0.132208 0.786055  4.12987
-# 14:     1138.7985: 0.0372908 0.0698496 0.131679 0.789030  3.97496
-# 15:     1138.7771: 0.0346322 0.0686342 0.127097 0.791620  4.07268
-# 16:     1138.7760: 0.0343408 0.0685777 0.130443 0.789571  4.06513
-# 17:     1138.7751: 0.0346158 0.0685543 0.129147 0.790478  4.06150
-# 18:     1138.7751: 0.0346092 0.0685688 0.129127 0.790487  4.06182
-# 19:     1138.7751: 0.0346079 0.0685661 0.129128 0.790485  4.06182
-# 
-# Final Estimate of the Negative LLH:
-#   LLH:  -2929.534    norm LLH:  -3.020138 
-# mu        omega       alpha1        beta1        shape 
-# 5.220192e-04 1.560025e-05 1.291283e-01 7.904854e-01 4.061824e+00 
-# 
-# R-optimhess Difference Approximated Hessian Matrix:
-#   mu         omega        alpha1         beta1         shape
-# mu     -9605288.786       2119893 -3.380175e+03  1.421115e+03 -1.798020e+02
-# omega   2119893.004 -347391089679 -2.992451e+07 -4.841374e+07 -1.371940e+06
-# alpha1    -3380.175     -29924505 -4.599349e+03 -5.342390e+03 -1.564956e+02
-# beta1      1421.115     -48413741 -5.342390e+03 -7.640047e+03 -2.119294e+02
-# shape      -179.802      -1371940 -1.564956e+02 -2.119294e+02 -1.006576e+01
-# attr(,"time")
-# Time difference of 0.03225803 secs
-# 
-# --- END OF TRACE ---
-#   
-#   
-#   Time to Estimate Parameters:
-#   Time difference of 0.126307 secs
 
 # Fit garch(1, 1) model to log returns
-fit_garch <- garchFit(~ garch(1, 1), data = WMT_df$LogReturns, cond.dist = "std")
+fit_garch <- garchFit(~ garch(1, 1), data = WMT_df$LogReturns, cond.dist = "norm")
 print(summary(fit_garch))
 
 # Title:
--- R/Fit_ARMA.R
+++ R/Fit_ARMA.R
@@ -17,31 +17,17 @@ WMT_df$Date <- as.Date(WMT_df$Date, format = "%Y-%m-%d")
 arma_fit <- auto.arima(WMT_df$LogReturns, seasonal = FALSE)
 print(summary(arma_fit))
 
-# Create data frame for test statistics
-ARMA_summary <- data.frame(
-    "Model" = c(
-    "MA1",
-    "Standard Error",
-    "sigma2",
-    "Log-Likelihood",
-    "AIC",
-    "AICc",
-    "BIC"
-  ),
-   "ARIMA(0,0,1) with zero mean" = c(
-    arma_fit$var.coef[1],
-    sqrt(diag(arma_fit$var.coef)),
-    arma_fit$sigma2,
-    arma_fit$loglik,
-    arma_fit$aic,
-    arma_fit$aicc,
-    arma_fit$bic
-  )
-)
-
-# Format the table using kable
-kable(ARMA_summary, 
-      format = "latex",
-      digits = 4,
-      align = c('l', 'r', 'r')) %>%
-  kable_styling(full_width = FALSE)
+# Series: WMT_df$LogReturns 
+# ARIMA(0,0,1) with zero mean 
+# 
+# Coefficients:
+#   ma1
+# -0.0666
+# s.e.   0.0312
+# 
+# sigma^2 = 0.0002266:  log likelihood = 2694.43
+# AIC=-5384.85   AICc=-5384.84   BIC=-5375.1
+# 
+# Training set error measures:
+#   ME       RMSE         MAE      MPE   MAPE      MASE         ACF1
+# Training set 0.0003912174 0.01504515 0.009658333 96.81537 110.64 0.6897203 -0.002686485
